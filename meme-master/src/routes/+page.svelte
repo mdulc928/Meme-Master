0ac0441e3e820d7893d3689a592d1c8a6a434180
mdulcio <mdulcio@visiostack.com>
2024-11-24 15:15:16 -0500

    feat: Created svelte project and added capacitor to it

---
+++ meme-master/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { clsx } from 'clsx';
+	let time = $state(1);
+	let interval = $state<number | undefined>();
+</script>
+
+<div class="flex flex-col items-center justify-center gap-3 p-2">
+	<h1 class="font-serif font-bold">Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+
+	<button
+		class={clsx(
+			'flex rounded-lg px-4 py-2 font-semibold text-white',
+			interval === undefined ? 'bg-blue-500' : 'bg-red-400'
+		)}
+		onclick={() => {
+			if (interval) {
+				window.clearInterval(interval);
+				interval = undefined;
+			} else {
+				interval = window.setInterval(() => {
+					time += 1;
+				}, 1000);
+			}
+		}}
+	>
+		{interval !== undefined ? 'Start' : 'Stop'}
+	</button>
+	{time} passed
+</div>
