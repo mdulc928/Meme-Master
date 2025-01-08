46fe70003dad0cf4cd1dcadb1c4b42533e2ee2f1
mdulcio <mdulcio@visiostack.com>
2025-01-07 20:58:57 -0500

    refactor: Remove parent build

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-	appId: 'com.mememaster.app',
-	appName: 'Meme Master',
-	webDir: './build/prerendered'
-};
-
-export default config;
